c1434c60db358ff3879390ed35961be6cc727072
wasiftak <wtak732@gmail.com>
2025-08-28 22:00:01 +0530

    Add discriminator model

--- model.py
+++ model.py
@@ -30,5 +30,32 @@ def build_generator():
 generator = build_generator()
 generator.summary()
 
-img = generator.predict(np.random.randn(4,128,1))  #generate 4 random images
-print(img)
+# img = generator.predict(np.random.randn(4,128,1))  #generate 4 random images
+# print(img)
+
+def build_discriminator():
+    model = Sequential()
+    model.add(Conv2D(32, 5, input_shape=(28, 28, 1)))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(64, 5))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(128, 5))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(256, 5))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.4))
+
+    model.add(Flatten())
+    model.add(Dropout(0.4))
+    model.add(Dense(1, activation='sigmoid'))   #1: fake, 0: real
+
+    return model
+
+discriminator = build_discriminator()
+discriminator.summary()
